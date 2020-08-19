1c32f90d849af6b47937ac361975b317b3068626
cirops <cirops@gmail.com>
2020-08-19 20:34:31 -0300

    Completing Challenge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  useEffect(() => {
+    api.get("/repositories").then((response) => {
+      setRepositories(response.data);
+    }, []);
+  });
+
   async function handleAddRepository() {
-    // TODO
+    const newRepo = {
+      title: `JS Repo ${Date.now()}`,
+      url: "https://github.com/cirops",
+      techs: ["Node", "JS", "NodeJS"],
+    };
+    api.post("/repositories", newRepo).then((response) => {
+      setRepositories([...repositories, response.data])
+    });
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`repositories/${id}`).then(() => {
+      setRepositories(repositories.filter(repo => repo.id !== id));
+    })
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repo) => {
+          return (
+            <li key={repo.id}>
+              {repo.title}
+              <button onClick={() => handleRemoveRepository(repo.id)}>Remover</button>
+            </li>
+          );
+        })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
